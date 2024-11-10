2fb703cab37f7591c05b9bc8a152c3dae2f0c446
istiakbinmahmod <istiak.mahmod@optimizely.com>
2024-11-10 06:15:11 +0600

    update snippt

--- app/layout.tsx
+++ app/layout.tsx
@@ -65,7 +65,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       className={`${space_grotesk.variable} scroll-smooth`}
       suppressHydrationWarning
     >
-      <Script src="https://cdn.optimizely.com/js/30296990906.js"></Script>
+      <Script src="https://optimizely-staging.s3.amazonaws.com/js/5290333368221696.js"></Script>
       <link rel="apple-touch-icon" sizes="76x76" href="/static/favicons/apple-touch-icon.png" />
       <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/favicon-16x16.png" />
