f5126b1e7ce23462a23a75992052615671e2df04
istiakbinmahmod <istiak.mahmod@optimizely.com>
2025-03-11 20:51:05 +0600

    snippet update

--- app/layout.tsx
+++ app/layout.tsx
@@ -66,6 +66,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       suppressHydrationWarning
     >
       <Script src="https://cdn.optimizely.com/js/5108571407384576.js"></Script>
+      {/* snippet link above */}
       <link rel="apple-touch-icon" sizes="76x76" href="/static/favicons/apple-touch-icon.png" />
       <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/favicon-16x16.png" />
